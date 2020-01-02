bdce4dab51f26f7d4c26f7989b312f596297a136
David Monrabal <dmonrabal@gmail.com>
2020-01-02 21:10:21 +0100

    Modificaciones de Iphone x+

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ importScripts('js/sw-utils.js');
  * Caches spaces
  */
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -52,6 +52,10 @@ self.addEventListener('activate', e => {
     const activate = caches.keys().then(keys => {
         keys.forEach(key => {
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
         });
